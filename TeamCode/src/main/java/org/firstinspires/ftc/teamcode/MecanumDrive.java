608997579b60117e56f67df719bce27087fdc483
Jb503 <jb-503@hotmail.com>
2023-10-27 19:08:32 -0700

    fixed IMU??

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -41,6 +41,12 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Quaternion;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 import java.lang.Math;
 import java.util.Arrays;
@@ -100,7 +106,67 @@ public final class MecanumDrive {
 
     public final VoltageSensor voltageSensor;
 
-    public final IMU imu = null;
+    public final IMU imu = new IMU() {
+        @Override
+        public boolean initialize(Parameters parameters) {
+            return false;
+        }
+
+        @Override
+        public void resetYaw() {
+
+        }
+
+        @Override
+        public YawPitchRollAngles getRobotYawPitchRollAngles() {
+            return null;
+        }
+
+        @Override
+        public Orientation getRobotOrientation(AxesReference reference, AxesOrder order, AngleUnit angleUnit) {
+            return null;
+        }
+
+        @Override
+        public Quaternion getRobotOrientationAsQuaternion() {
+            return null;
+        }
+
+        @Override
+        public AngularVelocity getRobotAngularVelocity(AngleUnit angleUnit) {
+            return null;
+        }
+
+        @Override
+        public Manufacturer getManufacturer() {
+            return null;
+        }
+
+        @Override
+        public String getDeviceName() {
+            return null;
+        }
+
+        @Override
+        public String getConnectionInfo() {
+            return null;
+        }
+
+        @Override
+        public int getVersion() {
+            return 0;
+        }
+
+        @Override
+        public void resetDeviceConfigurationForOpMode() {
+
+        }
+
+        @Override
+        public void close() {
+
+        }
+    };
 
     public final Localizer localizer;
     public Pose2d pose;
